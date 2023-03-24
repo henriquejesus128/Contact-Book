467d0afb103104ec69ee42facc360a2eb4d744e3
Henrique Jesus <henrique.jesus128@gmail.com>
2023-03-24 17:15:31 -0300

    feat component loading

---
+++ src/components/Loading/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { LoadingContainer, LoadingIcon } from "./style";
+
+const Loading = () => {
+  return (
+    <LoadingContainer>
+      <LoadingIcon />
+    </LoadingContainer>
+  );
+};
+
+export default Loading;
