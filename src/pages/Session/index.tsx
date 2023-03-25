14f406a7bca51fcb06f1034e770c62c66f67c78a
Henrique Jesus <henrique.jesus128@gmail.com>
2023-03-25 16:13:21 -0300

    fix session

--- src/pages/Session/index.tsx
+++ src/pages/Session/index.tsx
@@ -1,5 +1,22 @@
+import { useForm } from "react-hook-form";
+import { IReqSession } from "../../interface";
+import { yupResolver } from "@hookform/resolvers/yup";
+
 const Session = () => {
-  return;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IReqSession>({
+    resolver: yupResolver(),
+  });
+  return (
+    <>
+      <header></header>
+      <main></main>
+      <footer></footer>
+    </>
+  );
 };
 
 export default Session;
