c649e0edc0ce93816f047c205472ecf08aa826a1
Henrique Jesus <henrique.jesus128@gmail.com>
2023-03-25 18:21:24 -0300

    fix main components

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,11 +1,18 @@
-import { Outlet } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import Login from "../Login";
 import { MainStyle } from "./style";
 
 const Main = () => {
+  let location = useLocation();
   return (
     <MainStyle>
-      <Login />
+      {location.pathname === `/register` ? (
+        <h1>Regiter</h1>
+      ) : location.pathname === `/dashboard` ? (
+        <h1>DashBoard</h1>
+      ) : (
+        <Login />
+      )}
     </MainStyle>
   );
 };
