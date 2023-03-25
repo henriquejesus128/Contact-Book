f73685680e72331a8d1a6a7cb00b6277e64364c7
Henrique Jesus <henrique.jesus128@gmail.com>
2023-03-25 18:10:43 -0300

    fix routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,17 @@
 import { Route, Routes } from "react-router-dom";
-import ProtectedRoutes from "../components/ProtectedRoutes";
-import Dashboard from "../pages/Dashboard";
+// import ProtectedRoutes from "../components/ProtectedRoutes";
+// import Dashboard from "../pages/Dashboard";
 import Login from "../pages/Session";
 import NotFoundPage from "../pages/NotFoundPage";
-import Register from "../pages/Register";
+// import Register from "../pages/Register";
 
 const Routed = () => (
   <Routes>
     <Route path="/" element={<Login />} />
-    <Route path="/register" element={<Register />} />
+    {/* <Route path="/register" element={<Register />} />
     <Route element={<ProtectedRoutes />}>
       <Route path="/dashboard" element={<Dashboard />} />
-    </Route>
+    </Route> */}
     <Route path="*" element={<NotFoundPage />} />
   </Routes>
 );
