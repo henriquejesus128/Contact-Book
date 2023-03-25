439e0874cfc7817520cf26c65ad35786b8611df3
Henrique Jesus <henrique.jesus128@gmail.com>
2023-03-25 18:10:31 -0300

    feat style btn, form, input and label

---
+++ src/styles/Input/style.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const InputStyle = styled.input`
+  max-width: 100%;
+  height: 38.5px;
+
+  border-radius: 3.21px;
+  padding: 0px 13px;
+
+  background-color: var(--grey-2);
+
+  border: 1px solid transparent;
+
+  color: var(--grey-0);
+
+  outline: none;
+`;
