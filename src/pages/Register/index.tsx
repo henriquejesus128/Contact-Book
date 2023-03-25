870bf089608cf219d8e6811f124131260201b2a5
Henrique Jesus <henrique.jesus128@gmail.com>
2023-03-25 18:21:45 -0300

    fix Register

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,5 +1,13 @@
+import Header from "../../components/Header";
+import Main from "../../components/Main";
+
 const Register = () => {
-  return;
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
 };
 
 export default Register;
