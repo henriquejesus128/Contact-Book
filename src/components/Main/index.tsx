1e45d1f2cb948d79ab529c986f889ca14c39cf65
Henrique Jesus <henrique.jesus128@gmail.com>
2023-03-25 18:11:28 -0300

    fix Main component

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Login from "../Login";
+import { MainStyle } from "./style";
 
 const Main = () => {
   return (
     <MainStyle>
-      <Outlet />
+      <Login />
     </MainStyle>
   );
 };
