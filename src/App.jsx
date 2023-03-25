9ae83ab6c75156dbf0d8f26b3ac045af9d39c8d9
Henrique Jesus <henrique.jesus128@gmail.com>
2023-03-25 12:01:52 -0300

    fix app.jsx test

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import Loading from "./components/Loading";
-import { useContext } from "react";
+// import Loading from "./components/Loading";
+// import { useContext } from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Routed from "./routes";
 
 const App = () => {
-  const { loading } = useContext();
+  // const { loading } = useContext();
   return (
     <>
-      {loading && <Loading />}
+      {/* {loading && <Loading />} */}
       <ToastContainer />
       <Routed />
     </>
