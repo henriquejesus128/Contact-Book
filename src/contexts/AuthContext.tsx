2a4db69c917d402452383ebf862292e3c9a638ab
Henrique Jesus <henrique.jesus128@gmail.com>
2023-03-25 22:55:06 -0300

    fix auth context

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,52 +1,51 @@
-import axios from "axios";
 import { createContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import {
   IAuthContext,
   IProviderProps,
   IReqRegister,
-  IUser,
   IReqSession,
-  IToken,
 } from "../interface";
 import { instance } from "../services/axios";
 import { Erro, Success } from "../services/toast";
+import axios from "axios";
 
 export const AuthContext = createContext({} as IAuthContext);
 
 const AuthProvider = ({ children }: IProviderProps) => {
   const token = localStorage.getItem(`@ContactBook:token`);
   const id = localStorage.getItem(`@ContactBook:id`);
-
-  const [loading, setLoading] = useState<boolean>(false);
-
   const navigate = useNavigate();
+
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
 
   const register = async (data: IReqRegister) => {
     setLoading(true);
     try {
-      await instance.post<IUser>(`/users`, data);
+      await instance.post("/users", data);
       Success(`✅Usuário cadastrado com sucesso!`);
       navigate(`/`);
-    } catch (err) {
-      if (axios.isAxiosError(err)) {
-        Erro(`${err.message}❗❗`);
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        Erro(`${error.message}❗❗`);
       }
+    } finally {
+      setLoading(false);
     }
-    setLoading(false);
   };
 
   const session = async (data: IReqSession) => {
     setLoading(true);
     try {
-      const resp = await instance.post<IToken>(`/sessions`, data);
-      localStorage.setItem(`@ContactBook:token`, resp.data.token);
-      localStorage.setItem(`@ContactBook:id`, resp.data.user.id);
+      const resp = await instance.post("/session", data);
+      localStorage.setItem(`@KenzieHub:token`, resp.data.token);
+      localStorage.setItem(`@KenzieHub:id`, resp.data.user.id);
       Success(`✅Usuário logado com sucesso!`);
       navigate(`/dashboard`, { replace: true });
-    } catch (err) {
-      if (axios.isAxiosError(err)) {
-        Erro(`${err.message}❗❗`);
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        Erro(`${error.message}❗❗`);
       }
     } finally {
       setLoading(false);
@@ -57,6 +56,8 @@ const AuthProvider = ({ children }: IProviderProps) => {
       value={{
         token,
         id,
+        showPassword,
+        setShowPassword,
         loading,
         setLoading,
         register,
@@ -67,5 +68,4 @@ const AuthProvider = ({ children }: IProviderProps) => {
     </AuthContext.Provider>
   );
 };
-
 export default AuthProvider;
