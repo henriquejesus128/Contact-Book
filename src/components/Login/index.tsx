f82dfce9d4b2703c0cabee824e9f414cf94d564c
Henrique Jesus <henrique.jesus128@gmail.com>
2023-03-25 22:59:01 -0300

    fix component login

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -5,20 +5,35 @@ import { FormStyle } from "../../styles/Form/style";
 import { InputStyle } from "../../styles/Input/style";
 import { LabelStyle } from "../../styles/Label/style";
 import { ButtonStyle } from "../../styles/Button/style";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { sessionSchema } from "../../schemas/sessionSchema";
 
 const Login = () => {
-  const { showPassword, setShowPassword } = useContext(AuthContext);
+  const { showPassword, setShowPassword, session } = useContext(AuthContext);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(sessionSchema) });
   return (
-    <FormStyle>
+    <FormStyle onSubmit={handleSubmit(session)}>
       <h1>Login</h1>
       <LabelStyle htmlFor="email">Email</LabelStyle>
-      <InputStyle type="email" id="email" placeholder="Digite aqui seu email" />
+      <InputStyle
+        type="email"
+        id="email"
+        placeholder="Digite aqui seu email"
+        {...register(`email`)}
+      />
+      <span>{errors.email?message}</span>
       <LabelStyle htmlFor="password">Senha</LabelStyle>
       <div className="password">
         <InputStyle
           type="password"
           id="password"
           placeholder="Digite aqui sua senha"
+          {...register(`password`)}
         />
         {showPassword === false ? (
           <FaEyeSlash onClick={() => setShowPassword(true)} />
@@ -26,6 +41,7 @@ const Login = () => {
           <FaEye onClick={() => setShowPassword(false)} />
         )}
       </div>
+      <span>{errors.password?message}</span>
       <ButtonStyle type="submit">Entrar</ButtonStyle>
     </FormStyle>
   );
