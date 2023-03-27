c9e9d704d755e7843dbde2486e9440d5fd955441
Henrique Jesus <henrique.jesus128@gmail.com>
2023-03-26 22:20:14 -0300

    fix context user

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -1,27 +1,103 @@
-import { createContext, useContext, useState } from "react";
-import { IProviderProps, IUser, IUserContext } from "../interface";
+import { createContext, useContext, useState, useEffect } from "react";
+import { IPatchUser, IProviderProps, IUser, IUserContext } from "../interface";
 import { instance } from "../services/axios";
 import { AuthContext } from "./AuthContext";
+import { Erro, Success } from "../services/toast";
+import { useNavigate } from "react-router-dom";
 
 export const UserContext = createContext({} as IUserContext);
 
 const UserProvider = ({ children }: IProviderProps) => {
-  const { setLoading } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const { setLoading, token, id, setUser } = useContext(AuthContext);
   const [allUsers, setAllUsers] = useState<IUser[] | null>(null);
+
+  useEffect(() => {
+    instance.defaults.headers.common.authorization = `Bearer ${token}`;
+    if (token) {
+      getMyProfile();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const listUsers = async () => {
     setLoading(true);
     try {
       const { data } = await instance.get<IUser[]>("/users");
       setAllUsers(data);
+      Success(`✅Usuários listados com sucesso!`);
     } catch (error) {
-      console.error(error);
+      Erro("Falha ao listar todos os usuario❗❗");
     } finally {
       setLoading(false);
     }
   };
+
+  const getMyProfile = async () => {
+    instance.defaults.headers.common.authorization = `Bearer ${token}`;
+    try {
+      const { data } = await instance.get<IUser>(`/users/${id}`);
+      setUser(data);
+    } catch (error) {
+      Erro("Sessão expirada! Faça login novamente❗❗");
+      localStorage.clear();
+      navigate("/");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const getRetriverUser = async (id: number | string) => {
+    instance.defaults.headers.common.authorization = `Bearer ${token}`;
+    try {
+      const { data } = await instance.get<IUser>(`/users/${id}`);
+      setUser(data);
+    } catch (error) {
+      Erro("Não foi possivel encontrar esse usuario❗❗");
+      localStorage.clear();
+      navigate("/");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const patchUser = async (body: IPatchUser) => {
+    instance.defaults.headers.common.authorization = `Bearer ${token}`;
+    setLoading(true);
+    try {
+      const { data } = await instance.patch<IUser>("/users", body);
+      setUser(data);
+    } catch (error) {
+      Erro("Não foi possivel editar esse usuario❗❗");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const deleteUser = async () => {
+    instance.defaults.headers.common.authorization = `Bearer ${token}`;
+    setLoading(true);
+    try {
+      await instance.delete("/users");
+      await listUsers();
+    } catch (error) {
+      Erro("Não foi possivel desativar esse usuario❗❗");
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <UserContext.Provider value={{ listUsers, allUsers, setAllUsers }}>
+    <UserContext.Provider
+      value={{
+        listUsers,
+        allUsers,
+        setAllUsers,
+        getRetriverUser,
+        patchUser,
+        deleteUser,
+      }}
+    >
       {children}
     </UserContext.Provider>
   );
