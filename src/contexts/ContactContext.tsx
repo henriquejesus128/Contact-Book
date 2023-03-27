546f9e14125b9a8774a86919f1d9790bfa5c13dd
Henrique Jesus <henrique.jesus128@gmail.com>
2023-03-26 23:20:01 -0300

    fix contact context

---
+++ src/contexts/ContactContext.tsx
@@ -0,0 +1,115 @@
+import { createContext, useContext } from "react";
+import {
+  IContact,
+  IContactContext,
+  IPatchContact,
+  IProviderProps,
+  IReqContact,
+  IRespContact,
+} from "../interface";
+import { useState } from "react";
+import { AuthContext } from "./AuthContext";
+import { instance } from "../services/axios";
+import { Erro, Success } from "../services/toast";
+
+export const ContactContext = createContext({} as IContactContext);
+
+const ContactProvider = ({ children }: IProviderProps) => {
+  const [allcontact, setAllContact] = useState([] as IContact[]);
+  const [contact, setContact] = useState<IContact>({} as IContact);
+  const { setLoading, token, id } = useContext(AuthContext);
+
+  const listContacts = async () => {
+    instance.defaults.headers.common.authorization = `Bearer ${token}`;
+    setLoading(true);
+    try {
+      const { data } = await instance.get<IContact[]>("/contacts");
+      setAllContact(data);
+      Success(`✅Contatos listados com sucesso!`);
+    } catch (error) {
+      Erro("Falha ao listar todos os contatos❗❗");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const createContact = async (data: IReqContact) => {
+    instance.defaults.headers.common.authorization = `Bearer ${token}`;
+    setLoading(true);
+    const body = {
+      ...data,
+      userId: id,
+    };
+    try {
+      await instance.post<IRespContact>("/contacts", body);
+      Success(`✅Contatos criado com sucesso!`);
+      await listContacts();
+    } catch (error) {
+      Erro("Falha ao criar o contato❗❗");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const retriverContact = async (id: string) => {
+    instance.defaults.headers.common.authorization = `Bearer ${token}`;
+    setLoading(true);
+    try {
+      const { data } = await instance.get<IContact>(`/contacts/${id}`);
+      setContact(data);
+      Success(`✅Contato encontrado com sucesso!`);
+    } catch (error) {
+      Erro("Não foi possivel encontrar esse contato❗❗");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const patchContact = async (body: IPatchContact, id: string) => {
+    instance.defaults.headers.common.authorization = `Bearer ${token}`;
+    setLoading(true);
+    try {
+      const { data } = await instance.patch<IContact>(`contacts/${id}`, body);
+      setContact(data);
+      await listContacts();
+      Success(`✅Contatos editado com sucesso!`);
+    } catch (error) {
+      Erro("Não foi possivel editar esse usuario❗❗");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const deleteContact = async (id: string) => {
+    instance.defaults.headers.common.authorization = `Bearer ${token}`;
+    setLoading(true);
+    try {
+      await instance.delete(`/contacts/${id}`);
+      await listContacts();
+      Success(`✅Contatos deletado com sucesso!`);
+    } catch (error) {
+      Erro("Não foi possivel deletar esse contato❗❗");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <ContactContext.Provider
+      value={{
+        allcontact,
+        setAllContact,
+        contact,
+        setContact,
+        listContacts,
+        createContact,
+        retriverContact,
+        patchContact,
+        deleteContact,
+      }}
+    >
+      {children}
+    </ContactContext.Provider>
+  );
+};
+export default ContactProvider;
