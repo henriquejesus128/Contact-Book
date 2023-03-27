fe390056fda006a223f7900d09a88703fc8d12a2
Henrique Jesus <henrique.jesus128@gmail.com>
2023-03-27 00:49:49 -0300

    fix card contact

--- src/components/CardContact/index.tsx
+++ src/components/CardContact/index.tsx
@@ -4,19 +4,19 @@ import { IContactProps } from "../../interface";
 import { Headline, Title, CardShow, Li } from "./style";
 import img from "../../assets/image.png";
 
-const Card = ({ elem }: IContactProps) => {
-  const { setTec, setModalEdit } = useContext(ContactContext);
+const CardContact = ({ elem }: IContactProps) => {
+  const { setContact, setModalEdit } = useContext(ContactContext);
   return (
     <Li id={elem.id}>
-      <img src={elem.photo ? elem.phone : img} alt={elem.name} />
+      <img src={elem.photo ? elem.photo : img} alt={elem.name} />
       <Title>{elem.name}</Title>
       <Headline>{elem.email}</Headline>
       <Headline>{elem.phone}</Headline>
-      <CardShow onClick={() => [setModalEdit(true), setTec(elem)]}>
+      <CardShow onClick={() => [setModalEdit(true), setContact(elem)]}>
         Abrir
       </CardShow>
     </Li>
   );
 };
 
-export default Card;
+export default CardContact;
