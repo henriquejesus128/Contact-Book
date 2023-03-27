33ae49dccf0ffd335d2094cecdb763f6f20e49f9
Henrique Jesus <henrique.jesus128@gmail.com>
2023-03-26 21:31:36 -0300

    feat protected routes components

---
+++ src/components/ProtectedRoutes/index.tsx
@@ -0,0 +1,19 @@
+import { useContext } from "react";
+import { Navigate, Outlet } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthContext";
+import UserProvider from "../../contexts/UserContext";
+
+const ProtectedRoutes = () => {
+  const { token } = useContext(AuthContext);
+  return token ? (
+    <UserProvider>
+      <ContactProvider>
+        <Outlet />
+      </ContactProvider>
+    </UserProvider>
+  ) : (
+    <Navigate to="/" />
+  );
+};
+
+export default ProtectedRoutes;
