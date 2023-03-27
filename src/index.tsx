3b7390afe44778043af8ad13f523cdd8d62e82e5
Henrique Jesus <henrique.jesus128@gmail.com>
2023-03-27 10:53:06 -0300

    fix index tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import AuthProvider from "./contexts/AuthContext";
 import { GlobalStyle } from "./styles/global";
 
 const root = ReactDOM.createRoot(
@@ -10,8 +11,10 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <GlobalStyle />
-      <App />
+      <AuthProvider>
+        <GlobalStyle />
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
