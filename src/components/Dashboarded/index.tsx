7284b035e35fdcd14825c68b50b8132695a819c0
Henrique Jesus <henrique.jesus128@gmail.com>
2023-03-27 01:52:36 -0300

    fix dashboarded components

--- src/components/Dashboarded/index.tsx
+++ src/components/Dashboarded/index.tsx
@@ -7,16 +7,25 @@ import { UserContext } from "../../contexts/UserContext";
 import ListUser from "../ListUser";
 import ListContact from "../ListContact";
 import { AddContactTitle } from "../AddContact/style";
+import { ModalContainer } from "../../styles/Container/style";
+import { AuthContext } from "../../contexts/AuthContext";
+import img from "../../assets/image.png";
 
 const Dashboarded = () => {
-  const { allcontact } = useContext(ContactContext);
+  const { allcontact, modalEdit, setModalEdit } = useContext(ContactContext);
   const { allUsers } = useContext(UserContext);
+  const { user } = useContext(AuthContext);
   return (
-    <ContainerDash>
+    <ContainerDash onClick={() => [setModalEdit(true)]}>
+      {modalEdit && (
+        <ModalContainer>
+          <ModalEditContact />
+        </ModalContainer>
+      )}
       <DashSection>
-        <h1>Henrique</h1>
-        <p>Email</p>
-        <img src="" alt="" />
+        <h1>{user.name}</h1>
+        <p>{user.email}</p>
+        <img src={user.photo ? user.photo : img} alt={user.name} />
       </DashSection>
       <DashContDiv>
         <AddContact />
