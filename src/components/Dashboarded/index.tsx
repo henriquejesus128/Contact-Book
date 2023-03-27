16536567a25457917087e7b8c539a9bc4add174f
Henrique Jesus <henrique.jesus128@gmail.com>
2023-03-26 21:30:34 -0300

    feat component Dashboarded

---
+++ src/components/Dashboarded/index.tsx
@@ -0,0 +1,15 @@
+import { ContainerDash, DashContDiv, DashSection } from "./style";
+
+const Dashboarded = () => {
+  return (
+    <ContainerDash>
+      <DashSection>
+        <h1>Henrique</h1>
+        <p>Email</p>
+        <img src="" alt="" />
+      </DashSection>
+      <DashContDiv>Lista de contato</DashContDiv>
+    </ContainerDash>
+  );
+};
+export default Dashboarded;
