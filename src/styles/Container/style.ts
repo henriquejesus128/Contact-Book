94a8691e7a7eea01f24ab54114438f375b7b68c0
Henrique Jesus <henrique.jesus128@gmail.com>
2023-03-27 00:04:41 -0300

    fix modal container style

---
+++ src/styles/Container/style.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const ModalContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  background-color: rgba(0, 0, 0, 0.6);
+  top: 0;
+  right: 0;
+`;
