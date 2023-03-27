95c71b40af1ffd82679f48779da89813380c0e26
Henrique Jesus <henrique.jesus128@gmail.com>
2023-03-26 23:42:00 -0300

    feat could not find

---
+++ src/components/CouldNotFind/style.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const CouldNotFindStyle = styled.div`
+  height: 250px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const CouldNotFindTitle = styled.h2`
+  color: var(--grey-0);
+`;
