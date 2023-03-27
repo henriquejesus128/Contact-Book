83bc0a6fbc8e6cfde1d01f263d045c4c14004c38
Henrique Jesus <henrique.jesus128@gmail.com>
2023-03-27 10:52:33 -0300

    fix app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-// import { useContext } from "react";
+import { useContext } from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-// import Loading from "./components/Loading";
-// import { AuthProvider } from "./contexts/AuthContext";
+import Loading from "./components/Loading";
+import { AuthContext } from "./contexts/AuthContext";
 import Routed from "./routes";
 
 const App = () => {
-  // const { loading } = useContext(AuthProvider);
+  const { loading } = useContext(AuthContext);
   return (
     <>
-      {/* {loading && <Loading />} */}
+      {loading && <Loading />}
       <ToastContainer />
       <Routed />
     </>
