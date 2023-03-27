2d63f7502f2a4f625d65ef4158336cf996130ea8
Henrique Jesus <henrique.jesus128@gmail.com>
2023-03-27 00:43:04 -0300

    feat components card

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,21 +1,21 @@
 import { useContext } from "react";
 import { ContactContext } from "../../contexts/ContactContext";
 import { IContactProps } from "../../interface";
-import { CardTecHeadline, CardTecLi, CardTecShow, CardTecTitle } from "./style";
+import { Headline, Title, CardShow, Li } from "./style";
 import img from "../../assets/image.png";
 
 const Card = ({ elem }: IContactProps) => {
   const { setTec, setModalEdit } = useContext(ContactContext);
   return (
-    <CardTecLi id={elem.id}>
+    <Li id={elem.id}>
       <img src={elem.photo ? elem.phone : img} alt={elem.name} />
-      <CardTecTitle>{elem.name}</CardTecTitle>
-      <CardTecHeadline>{elem.email}</CardTecHeadline>
-      <CardTecHeadline>{elem.phone}</CardTecHeadline>
-      <CardTecShow onClick={() => [setModalEdit(true), setTec(elem)]}>
+      <Title>{elem.name}</Title>
+      <Headline>{elem.email}</Headline>
+      <Headline>{elem.phone}</Headline>
+      <CardShow onClick={() => [setModalEdit(true), setTec(elem)]}>
         Abrir
-      </CardTecShow>
-    </CardTecLi>
+      </CardShow>
+    </Li>
   );
 };
 
--- src/components/Card/style.ts
+++ src/components/Card/style.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const CardTecLi = styled.li`
+export const Li = styled.li`
   background-color: var(--grey-4);
   width: 90%;
   min-height: 50px;
@@ -11,30 +11,17 @@ export const CardTecLi = styled.li`
   padding: 0.2rem;
   border-radius: 4px;
 `;
-export const CardTecTitle = styled.h2`
+export const Title = styled.h2`
   max-width: 100px;
   color: var(--grey-0);
   margin-left: 1rem;
   text-overflow: ellipsis;
 `;
-export const CardTecHeadline = styled.p`
+export const Headline = styled.p`
   color: var(--grey-1);
 `;
-export const CardTecCont = styled.div`
-  height: 100%;
-  width: 150px;
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-end;
-  align-items: center;
-  gap: 1rem;
-  svg {
-    display: none;
-    color: var(--grey-0);
-    cursor: pointer;
-  }
-`;
-export const CardTecShow = styled.button`
+
+export const CardShow = styled.button`
   display: flex;
   align-items: center;
   color: var(--grey-0);
